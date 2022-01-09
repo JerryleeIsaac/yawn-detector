5b4db95f93bcf0782cfe7f612db8af4192da6fa4
Jerrylee Isaac Geronimo <jerryleegeronimo@gmail.com>
2022-01-09 22:51:10 +0800

    Initial results

--- src/pipelines/train.py
+++ src/pipelines/train.py
@@ -1,6 +1,4 @@
-
-from os import sched_getscheduler
-from sklearn import model_selection
+import time
 import torch
 import torchvision as tv
 import argparse
@@ -14,6 +12,9 @@ from sklearn.metrics import (
 from loguru import logger
 from zipfile import ZipFile
 import shutil
+import os
+import json
+from tqdm import tqdm
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -26,16 +27,20 @@ def load_model(
 
     logger.info(f"Loading model {model_type} with {model_params}")
     if model_type == "MobileNetV3":
-        model = tv.models.mobilenet_v3_large(pretrained=model_params["pretrained"])
-	
+        model = tv.models.mobilenet_v3_large(
+            pretrained=model_params["pretrained"])
+        model.classifier[-1] = torch.nn.Linear(1280, 2)
+
     model = model.to(device)
 
     return model
+
 
 def load_criterion(model_criterion):
     logger.info(f"Loading criterion {model_criterion}")
     if model_criterion == "CrossEntropy":
         return torch.nn.CrossEntropyLoss()
+
 
 def load_optimizer(trainable_params, optimizer_type, **optimizer_params):
     logger.info(f"Loading optimizer {optimizer_type} with {optimizer_params}")
@@ -54,6 +59,7 @@ def load_optimizer(trainable_params, optimizer_type, **optimizer_params):
 
     return optimizer
 
+
 def load_scheduler(optimizer, scheduler_type, **scheduler_params):
     logger.info(f"Loading scheduler {scheduler_type} with {scheduler_params}")
 
@@ -69,9 +75,8 @@ def load_scheduler(optimizer, scheduler_type, **scheduler_params):
             factor=scheduler_params["factor"],
             patience=scheduler_params["patience"]
         )
-    
-    return scheduler
 
+    return scheduler
 
 
 def get_metrics(y_actuals, y_preds):
@@ -84,51 +89,70 @@ def get_metrics(y_actuals, y_preds):
 
 
 def train_model(data_loader, model, criterion, optimizer, scheduler, device):
-	model.train()
+    y_actuals, y_preds = [], []
 
-	y_actuals, y_preds = [], []
+    losses = []
+    for inputs, labels in tqdm(data_loader, total=len(data_loader)):
+        inputs = inputs.to(device, dtype=torch.float)
+        labels = labels.to(device)
 
-	for i, (inputs, labels) in enumerate(data_loader):
-		inputs = inputs.to(device, dtype=torch.float)
-		labels = labels.to(device)
 
-		logger.info(f"Batch: {i}, Images: {len(inputs)} ")
+        optimizer.zero_grad()
 
-		optimizer.zero_grad()
+        with torch.set_grad_enabled(True):
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
 
-		with torch.set_grad_enabled(True):
-			outputs = model(inputs)
-			_, preds = torch.max(outputs, 1)
-			loss = criterion(outputs, labels)
+            loss.backward()
+            losses.append(loss.item())
+            optimizer.step()
 
-			loss.backward()
-			optimizer.step()
+            y_actuals.extend(labels.cpu().numpy().tolist())
+            y_preds.extend(preds.data.cpu().numpy().tolist())
 
-			y_actuals.extend(preds.cpu().numpy().tolist())
-			y_preds.extend(labels.data.cpu().numpy().tolist())
-		logger.debug("Batch run done")
+    epoch_results = get_metrics(y_actuals, y_preds)
+    learning_rate = optimizer.param_groups[0]["lr"]
 
-	epoch_results = get_metrics(y_actuals, y_preds)
-	learning_rate = optimizer.param_groups[0]["lr"]
+    scheduler.step(epoch_results["f1"])
+    logger.info(f"Epoch Train results: {epoch_results}, learning rate: {learning_rate}")
 
-	scheduler.step(epoch_results["f1"])
-	logger.info(f"Epoch results: {epoch_results}, learning rate: {learning_rate}")
+    return epoch_results, losses
 
-	return epoch_results
+def evaluate_model(data_loader, model, device):
+    model.eval()
+
+    y_actuals, y_preds = [], []
+    for inputs, labels in tqdm(data_loader, total=len(data_loader)):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        with torch.set_grad_enabled(False):
+            outputs = model(inputs)
+            probs, preds = torch.max(outputs, 1)
+
+        y_actuals.extend(labels.cpu().numpy().tolist())
+        y_preds.extend(preds.cpu().numpy().tolist())
+    
+    epoch_results = get_metrics(y_actuals, y_preds)
+
+    logger.info(f"Epoch Test results: {epoch_results}")
+
+    return epoch_results
 
 def get_transforms(input_size):
 
     imagenet_mean, imagenet_std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
     return {
         "train": tv.transforms.Compose([
-            tv.transforms.Resize(input_size),
+            tv.transforms.Resize((input_size, input_size)),
             tv.transforms.RandomHorizontalFlip(),
             tv.transforms.RandomVerticalFlip(),
             tv.transforms.ToTensor(),
             tv.transforms.Normalize(imagenet_mean, imagenet_std)
         ]),
         "test": tv.transforms.Compose([
-            tv.transforms.Resize(input_size),
+            tv.transforms.Resize((input_size, input_size)),
             tv.transforms.ToTensor(),
             tv.transforms.Normalize(imagenet_mean, imagenet_std)
         ]),
@@ -140,6 +164,46 @@ def unzip_data(data_zip):
     with ZipFile(data_zip, "r") as zip:
         zip.extractall()
     
+def clean_data_files(data_dir):
+    logger.info(f"Cleaning {data_dir}")
+    shutil.rmtree(data_dir)
+
+def get_dataloader(data_dir, transforms, batch_size, n_workers):
+    logger.info(f"Getting data loader for {data_dir}")
+    dataset = tv.datasets.ImageFolder(data_dir, transform=transforms)
+    return torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        num_workers=n_workers,
+    )
+
+def save_results(results, results_dir, results_file):
+    os.makedirs(results_dir, exist_ok=True) 
+    logger.info(f"Saving results to {results_file}")
+
+    with open(results_file, "w") as f:
+        json.dump(results, f)
+
+def save_losses(losses, results_dir, losses_file):
+    os.makedirs(results_dir, exist_ok=True) 
+    logger.info(f"Saving losses to {losses_file}")
+
+    with open(losses_file, "w") as f:
+        json.dump(losses, f)
+
+def save_plots(plots, results_dir, plots_file):
+    os.makedirs(results_dir, exist_ok=True) 
+    logger.info(f"Saving plots to {plots_file}")
+
+    with open(plots_file, "w") as f:
+        json.dump(plots, f)
+
+    
+def save_model(weights, model_dir, model_file):
+    os.makedirs(model_dir, exist_ok=True)
+    logger.info(f"Saving model weights to {model_file}")
+    torch.save(weights, model_file)
+
 def main(config):
 
     model = load_model(config["model_type"], DEVICE, **config["model_params"])
@@ -147,12 +211,69 @@ def main(config):
     criterion = load_criterion(config["model_criterion"])
     scheduler = load_scheduler(optimizer, config["scheduler_type"], **config["scheduler_params"])
 
-    # unzip_data(config["train_data_files_zip"])
-    # unzip_data(config["test_data_files_zip"])
+    unzip_data(config["train_data_files_zip"])
+    unzip_data(config["test_data_files_zip"])
 
-    # shutil.rmtree(config["train_data_files_dir"])
-    # shutil.rmtree(config["test_data_files_dir"])
+    transforms = get_transforms(config["input_size"])
+    train_dataloader = get_dataloader(
+        config["train_data_files_dir"],
+        transforms["train"],
+        config["batch_size"],
+        config["n_workers"]
+    )
+    test_dataloader = get_dataloader(
+        config["test_data_files_dir"],
+        transforms["test"],
+        config["batch_size"],
+        config["n_workers"]
+    )
 
+    os.makedirs(config["model_dir"], exist_ok=True)
+    best_score = -1
+    train_results = []
+    test_results = []
+    losses = []
+    best_weights = model.state_dict()
+
+    since = time.time()
+    for _ in range(config["epochs"]):
+        train_result, losses = train_model(train_dataloader, model, criterion, optimizer, scheduler, DEVICE)
+        losses.extend(losses)
+        train_results.append(train_result)
+
+        test_result = evaluate_model(test_dataloader, model, DEVICE)
+        test_results.append(test_result)
+
+        if test_result["f1"] > best_score:
+            logger.info(f"Current score {test_result['f1']} exceeds best score {best_score}")
+            best_score = test_result["f1"]
+            best_weights = model.state_dict() 
+            save_model(best_weights, config["model_dir"], config["model_file"])
+
+            model.load_state_dict(best_weights)
+    time_elapsed = time.time() - since
+    logger.info(f"Training completed in {time_elapsed / 60}m {time_elapsed % 60}s")
+
+    model.load_state_dict(best_weights)
+
+    logger.info("Evaluating model")
+    train_result = evaluate_model(train_dataloader, model, DEVICE)
+    train_result["time_elapsed"] = time_elapsed
+    test_result = evaluate_model(test_dataloader, model, DEVICE)
+
+    logger.info("Saving results")
+    save_results(train_result, config["results_dir"], config["train_results_file"])
+    save_results(test_result, config["results_dir"], config["test_results_file"])
+
+    logger.info("Saving data plots")
+    save_plots(train_results, config["results_dir"], config["train_plots_file"])
+    save_plots(test_results, config["results_dir"], config["test_plots_file"])
+    save_losses(losses, config["results_dir"], config["losses_file"])
+
+    save_model(model.state_dict(), config["model_dir"], config["model_file"])
+    
+    clean_data_files(config["train_data_files_dir"])
+    clean_data_files(config["test_data_files_dir"])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Trains model")
