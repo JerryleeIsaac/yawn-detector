68ce34d10fd1ae45b4d2d53e5244a92de6e8e50e
Jerrylee Isaac Geronimo <jerryleegeronimo@gmail.com>
2022-01-09 21:32:19 +0800

    Initial train dvc pipeline

--- src/pipelines/train.py
+++ src/pipelines/train.py
@@ -1,85 +1,157 @@
+
+from os import sched_getscheduler
+from sklearn import model_selection
+import torch
+import torchvision as tv
 import argparse
-import json
-import os
-import time
-
-import joblib
-import pandas as pd
 import yaml
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+)
 from loguru import logger
+from zipfile import ZipFile
+import shutil
 
-from src.models.nbsvm import NBSVM
-
-
-def load_data(config):
-    data_file = config.get("data_file")
-    logger.info(f"Loading data from {data_file}")
-    compression = None
-    if data_file.endswith(".gz"):
-        compression = "gzip"
-
-    data_df = pd.read_csv(data_file, compression=compression)
-
-    return data_df
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-def load_model(config):
-    logger.info("Loading model")
-    model_type = config.get("model_type")
-    if model_type != "nbsvm":
-        raise Exception("Unsupported model")
+def load_model(
+		model_type,
+        device="cpu",
+        **model_params
+	):
 
-    model_params = config.get("model_params")
-    if not model_params:
-        model_params = {}
-    model = NBSVM(**model_params)
+    logger.info(f"Loading model {model_type} with {model_params}")
+    if model_type == "MobileNetV3":
+        model = tv.models.mobilenet_v3_large(pretrained=model_params["pretrained"])
+	
+    model = model.to(device)
 
     return model
 
+def load_criterion(model_criterion):
+    logger.info(f"Loading criterion {model_criterion}")
+    if model_criterion == "CrossEntropy":
+        return torch.nn.CrossEntropyLoss()
 
-def train_model(model, data_df):
-    logger.info("Training model")
+def load_optimizer(trainable_params, optimizer_type, **optimizer_params):
+    logger.info(f"Loading optimizer {optimizer_type} with {optimizer_params}")
 
-    x = data_df["text"]
-    y = data_df["sentiment"]
+    if optimizer_type == "SGD":
+        optimizer = torch.optim.SGD(
+            trainable_params,
+            lr=optimizer_params["learning_rate"],
+            momentum=optimizer_params["momentum"]
+        )
+    elif optimizer_type == "Adam":
+        optimizer = torch.optim.Adam(
+            trainable_params,
+            lr=optimizer_params["learning_rate"]
+        )
 
-    start_ts = time.time()
-    model.fit(x, y)
-    end_ts = time.time()
-    train_duration = end_ts - start_ts
-    logger.info(f"Training took {train_duration} seconds")
+    return optimizer
 
-    train_results = {
-        "duration": train_duration,
+def load_scheduler(optimizer, scheduler_type, **scheduler_params):
+    logger.info(f"Loading scheduler {scheduler_type} with {scheduler_params}")
+
+    if scheduler_type == "StepLR":
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer,
+            step_size=scheduler_params["step_size"],
+            gamma=scheduler_params["gamma"]
+        )
+    elif scheduler_type == "ReduceLROnPlateau":
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            factor=scheduler_params["factor"],
+            patience=scheduler_params["patience"]
+        )
+    
+    return scheduler
+
+
+
+def get_metrics(y_actuals, y_preds):
+	return {
+		"accuracy": accuracy_score(y_actuals, y_preds),
+		"precision": precision_score(y_actuals, y_preds),
+		"recall": recall_score(y_actuals, y_preds),
+		"f1": f1_score(y_actuals, y_preds),
+	}
+
+
+def train_model(data_loader, model, criterion, optimizer, scheduler, device):
+	model.train()
+
+	y_actuals, y_preds = [], []
+
+	for i, (inputs, labels) in enumerate(data_loader):
+		inputs = inputs.to(device, dtype=torch.float)
+		labels = labels.to(device)
+
+		logger.info(f"Batch: {i}, Images: {len(inputs)} ")
+
+		optimizer.zero_grad()
+
+		with torch.set_grad_enabled(True):
+			outputs = model(inputs)
+			_, preds = torch.max(outputs, 1)
+			loss = criterion(outputs, labels)
+
+			loss.backward()
+			optimizer.step()
+
+			y_actuals.extend(preds.cpu().numpy().tolist())
+			y_preds.extend(labels.data.cpu().numpy().tolist())
+		logger.debug("Batch run done")
+
+	epoch_results = get_metrics(y_actuals, y_preds)
+	learning_rate = optimizer.param_groups[0]["lr"]
+
+	scheduler.step(epoch_results["f1"])
+	logger.info(f"Epoch results: {epoch_results}, learning rate: {learning_rate}")
+
+	return epoch_results
+
+def get_transforms(input_size):
+
+    imagenet_mean, imagenet_std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+    return {
+        "train": tv.transforms.Compose([
+            tv.transforms.Resize(input_size),
+            tv.transforms.RandomHorizontalFlip(),
+            tv.transforms.RandomVerticalFlip(),
+            tv.transforms.ToTensor(),
+            tv.transforms.Normalize(imagenet_mean, imagenet_std)
+        ]),
+        "test": tv.transforms.Compose([
+            tv.transforms.Resize(input_size),
+            tv.transforms.ToTensor(),
+            tv.transforms.Normalize(imagenet_mean, imagenet_std)
+        ]),
     }
 
-    logger.info(f"Train results: {train_results}")
 
-    return train_results
-
-
-def save_model(model, config):
-    model_dir = config.get("model_dir")
-    model_file = config.get("model_file")
-
-    os.makedirs(model_dir, exist_ok=True)
-    logger.info(f"Saving model to {model_file}")
-    joblib.dump(model, model_file)
-
-
-def save_results(train_results, config):
-    os.makedirs(config.get("results_dir"), exist_ok=True)
-    with open(config.get("results_file"), "w") as f:
-        json.dump(train_results, f)
-
-
+def unzip_data(data_zip):
+    logger.info(f"Unzipping {data_zip}")
+    with ZipFile(data_zip, "r") as zip:
+        zip.extractall()
+    
 def main(config):
 
-    data_df = load_data(config)
-    model = load_model(config)
-    train_results = train_model(model, data_df)
-    save_model(model, config)
-    save_results(train_results, config)
+    model = load_model(config["model_type"], DEVICE, **config["model_params"])
+    optimizer = load_optimizer(model.parameters(), config["optimizer_type"], **config["optimizer_params"])
+    criterion = load_criterion(config["model_criterion"])
+    scheduler = load_scheduler(optimizer, config["scheduler_type"], **config["scheduler_params"])
+
+    # unzip_data(config["train_data_files_zip"])
+    # unzip_data(config["test_data_files_zip"])
+
+    # shutil.rmtree(config["train_data_files_dir"])
+    # shutil.rmtree(config["test_data_files_dir"])
 
 
 if __name__ == "__main__":
